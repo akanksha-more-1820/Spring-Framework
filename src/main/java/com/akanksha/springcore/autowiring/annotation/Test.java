c742709b6fe1532c3ee153dd43e9d00376a403e1
akanksha-more-1820 <akankshamore1820@gmail.com>
2023-07-01 17:44:21 +0530

    Am-01 Autowiring through annotations

---
+++ src/main/java/com/akanksha/springcore/autowiring/annotation/Test.java
@@ -0,0 +1,13 @@
+package com.akanksha.springcore.autowiring.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("autowiringconfig.xml");
+        Emp emp=context.getBean("Emp", Emp.class);
+        System.out.println(emp);
+
+    }
+}
