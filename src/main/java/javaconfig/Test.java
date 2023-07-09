0cb1c44539267ba80863646cd3a70f902e7dce8c
akanksha-more-1820 <akankshamore1820@gmail.com>
2023-07-09 20:07:36 +0530

    Am-01 stereotype

--- src/main/java/javaconfig/Test.java
+++ src/main/java/javaconfig/Test.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test {
     public static void main(String[] args) {
         ApplicationContext context=new AnnotationConfigApplicationContext(JavaConfig.class);
-        Student st=context.getBean("temp", Student.class);
+         Student st=context.getBean("temp", Student.class);
         System.out.println(st);
         st.display();
     }
